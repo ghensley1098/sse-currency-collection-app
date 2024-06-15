ba047fe3caa16a39bcb82a4cb8c6e1ba292d38a1
GHensley <gmhensley26@gmail.com>
2024-06-15 19:45:48 -0400

    added back missing signup form info and corrected user database schema

--- venv/Scripts/penny_pinchers/polls/urls.py
+++ venv/Scripts/penny_pinchers/polls/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
+from .views import SignUpView
 
 from . import views
 
 app_name = "polls"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="IndexView"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
