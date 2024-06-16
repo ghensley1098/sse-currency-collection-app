6ce0b1bf4233bdc6f926ff41ad932b180242006d
Cec3 <Camo32003@gmail.com>
2024-06-16 03:24:31 -0400

    Completed UI for Login and Homepage

--- venv/Scripts/penny_pinchers/polls/urls.py
+++ venv/Scripts/penny_pinchers/polls/urls.py
@@ -3,7 +3,6 @@ from .views import SignUpView
 
 from . import views
 
-app_name = "polls"
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', views.custom_login_view, name='login'),
