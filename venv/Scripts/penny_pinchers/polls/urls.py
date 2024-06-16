6a72cdd72928f7ce2e5fdee655dfcc310402fb5d
Nazar99991 <lethbridgenathan@gmail.com>
2024-06-16 02:16:15 -0400

    Moved entries and collections into admin view

--- venv/Scripts/penny_pinchers/polls/urls.py
+++ venv/Scripts/penny_pinchers/polls/urls.py
@@ -3,9 +3,10 @@ from .views import SignUpView
 
 from . import views
 
-app_name = "polls"
+app_name = "mcollections"
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', views.custom_login_view, name='login'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    
 ]
